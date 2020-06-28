ab7ba15d291688fd06eaabda75742ca5e2bed83a
Jacopo Mangiavacchi <jmangia@me.com>
2020-06-28 04:08:46 -0700

    skeleton

---
+++ Shared/MNIST_ComputeMLApp.swift
@@ -0,0 +1,17 @@
+//
+//  MNIST_ComputeMLApp.swift
+//  Shared
+//
+//  Created by Jacopo Mangiavacchi on 6/28/20.
+//
+
+import SwiftUI
+
+@main
+struct MNIST_ComputeMLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
